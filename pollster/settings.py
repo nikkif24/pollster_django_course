ee9830287059f7b344cf70d5656a131aa0646e4b
nikkif24 <nicole.flores@getordermark.com>
2022-03-31 13:05:40 -0700

    sqlite3 to postgres

--- pollster/settings.py
+++ pollster/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'pollster.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'my_db',
+        'USER': 'nikki',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -120,3 +124,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Primary Key
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
